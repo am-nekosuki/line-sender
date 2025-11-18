d36a4a56be0cd4444f4d2bae57b2c4229c1ae967
am-nekosuki <7nk.no.dri@gmail.com>
2025-11-18 14:35:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -139,5 +139,5 @@ app.get('/send', async (req, res) => {
 // ③ Render 必須：PORT を listen してサーバーを起動
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(\`Server running on port \${PORT}\`);
+  console.log(`Server running on port ${PORT}`);
 });
